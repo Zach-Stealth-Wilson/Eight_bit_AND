7359469004f83094dc0e5fea35047cee5f043f6d
Zach-Stealth-Wilson <123966207+Zach-Stealth-Wilson@users.noreply.github.com>
2024-06-18 10:53:48 -0500

    Update and.v

--- src/and.v
+++ src/and.v
@@ -1,5 +1,5 @@
 /*
-A simple 8-bit AND in Verilog, TOP. 
+This circuit needs to accept 4 different two two-bit values, A and B. It needs to output 4 different one-bit values, for each gate. AND, NAND, XOR, and XNOR.. 
 */
 
 module tt_um_and(
@@ -7,9 +7,24 @@ module tt_um_and(
   input reset,
   input [7:0] a, b,
   output [7:0] Y
+ 
 );
+  wire n1;
+  wire n2;
+  
+  assign Y[0] = a[0] & b[0];// AND
 
-  assign Y = a & b;
+  assign n1 = a[1] & b[1];
+
+  assign Y[1] = ~(n1); // inversion
+
+  assign Y[2] = a[2] ^ b[2]; // XOR
+  
+  assign n2= a[3] ^ b[3];
+
+  assign Y[3] = ~(n2); //inversion
+
+  assign Y[7:4] = 4'b0000;
 
        
 
