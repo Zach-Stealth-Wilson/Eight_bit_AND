50067375d9387ae6a64dcdf00b7a61f1b91a94da
Zach-Stealth-Wilson <123966207+Zach-Stealth-Wilson@users.noreply.github.com>
2024-07-03 10:48:58 -0500

    Update and rename and.v to and.sv

--- src/and.sv
+++ src/and.sv
@@ -3,10 +3,10 @@ A simple 8-bit AND in Verilog, TOP.
 */
 
 module tt_um_and(
-  input clk,
-  input reset,
-  input [7:0] a, b,
-  output [7:0] Y
+  input logic clk,
+  input logic reset,
+  input logic [7:0] a, b,
+  output logic [7:0] Y
 );
 
   assign Y = a & b;
